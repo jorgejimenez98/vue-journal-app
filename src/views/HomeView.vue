48b2dd31ee903b3b3728ed5aa0c7512822542587
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-03-19 23:33:41 +0100

    Init sass on main app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" />
   </div>
-</template>
 
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Primary</button>
+  <button type="button" class="btn btn-warning">Primary</button>
+</template>
