166a092383a3d4fdd52118c00d6a73d005cb5490
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-03-19 23:58:24 +0100

    Add font awesome, create navbar and entry list

---
+++ src/modules/daybook/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar bg-primary">
+    <a class="navbar-brand text-white">
+      <img
+        alt="Vue Logo"
+        src="@/assets/logo.png"
+        class="d-inline-block align-text-top mx-2"
+        height="24"
+      />
+      DayBook
+    </a>
+
+    <div class="d-flex">
+        <button class="btn btn-info mr-5">
+            <i class="fa fa-sign-out-alt"></i>
+        </button>
+    </div>
+  </nav>
+</template>
--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>Daybook Layout</h1>
+  <NavBar />
+
+  <div class="d-flex">
+    <div class="col-4">
+      <EntryList />
+    </div>
+    <div class="col">
+      <router-view />
+    </div>
+  </div>
 </template>
+
+<script>
+import NavBar from "../components/NavBar.vue";
+import EntryList from "../components/EntriList.vue";
+export default {
+  components: {
+    NavBar,
+    EntryList,
+  },
+};
+</script>
