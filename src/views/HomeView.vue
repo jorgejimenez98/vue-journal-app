827f6539ebc88ab946211f23d9f662bff76e6fb5
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-03-19 23:45:52 +0100

    Create module structure and load daybook routers

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,17 @@
     <img alt="Vue logo" src="../assets/logo.png" />
   </div>
 
-  <button type="button" class="btn btn-primary">Primary</button>
+  <button @click="goToDaybook" type="button" class="btn btn-primary">Day Book</button>
   <button type="button" class="btn btn-secondary">Primary</button>
   <button type="button" class="btn btn-warning">Primary</button>
 </template>
+
+<script>
+export default {
+  methods: {
+    goToDaybook() {
+      this.$router.push({ name: "daybook" });
+    },
+  },
+};
+</script>
