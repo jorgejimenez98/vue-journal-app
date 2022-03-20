bdf05a6140bc525cef1185876531a02f699cf3ea
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-03-20 22:51:24 +0100

    Set loader to layout

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,7 +1,16 @@
 <template>
   <NavBar />
 
-  <div class="d-flex">
+  <div v-if="isLoading" class="row justify-content-md-center">
+    <div class="col-3 alert-info text-center mt-5">
+      Espere por favor...
+      <h3 class="mt-2">
+        <i class="fa fa-sign fa-sync"></i>
+      </h3>
+    </div>
+  </div>
+
+  <div v-else class="d-flex">
     <div class="col-4">
       <EntryList />
     </div>
@@ -14,10 +23,20 @@
 <script>
 import NavBar from "../components/NavBar.vue";
 import EntryList from "../components/EntriList.vue";
+import { mapActions, mapState } from "vuex";
 export default {
   components: {
     NavBar,
     EntryList,
+  },
+  computed: {
+    ...mapState("journal", ["isLoading"]),
+  },
+  methods: {
+    ...mapActions("journal", ["loadEntries"]),
+  },
+  created() {
+    this.loadEntries();
   },
 };
 </script>
