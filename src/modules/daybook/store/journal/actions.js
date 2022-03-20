78df19d15667a1cd2f068a55804cc639e63009c3
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-03-20 22:50:48 +0100

    Create loadentries mutation

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -1,4 +1,16 @@
-export const loadEntries = async () => {};
+import journalApi from "@/api/journalApi";
+
+export const loadEntries = async ({ commit }) => {
+  const { data } = await journalApi.get("/entries.json");
+  const entries = [];
+  for (let id of Object.keys(data)) {
+    entries.push({
+      id,
+      ...data[id],
+    });
+  }
+  commit("setEntries", entries);
+};
 
 export const updateEntries = async () => {};
 
