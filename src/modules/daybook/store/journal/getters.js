f21c11dd36765577fc5c27e1d16cef8037d24d40
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-03-20 22:20:14 +0100

    Create getters functions

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -1,2 +1,18 @@
-export const getEntriesByTerm = (/* state */) => {};
-export const getEntriesById = (/* state */) => {};
+export const getEntriesByTerm =
+  (state) =>
+  (term = "") => {
+    if (term.length === 0) {
+      return state.entries;
+    }
+    return state.entries.filter((item) =>
+      item.text.toLowerCase().includes(term.toLocaleLowerCase())
+    );
+  };
+
+export const getEntriesById =
+  (state) =>
+  (id = "") => {
+    const item = state.entries.find((item) => item.id === id);
+    if (!item) return;
+    return { ...item };
+  };
