e1e2df688539933b7a8f32cf06b25613588804fe
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-03-20 01:03:31 +0100

    Add two clildren routers on daybook routes

--- src/modules/daybook/router/index.js
+++ src/modules/daybook/router/index.js
@@ -2,6 +2,16 @@ export default {
     name: 'daybook',
     component: () => import(/* webpackChunkName: "daybook" */ '../layouts/DayBookLayout.vue'),
     children: [
-
+        /* Children views */
+        {
+            path: '',
+            name: 'no-entry',
+            component: () => import(/* webpackChunkName: "daybook-no-entry" */ '../views/NoEntrySelected.vue'),
+        },
+        {
+            path: ':id',
+            name: 'entry',
+            component: () => import(/* webpackChunkName: "daybook-entry" */ '../views/EntryView.vue'),
+        }
     ]
 }
