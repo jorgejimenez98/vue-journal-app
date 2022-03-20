4104cb7e7d33a7a76bc90291b5495055f5abf87d
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-03-20 01:02:14 +0100

    Create no-entry-selected component

---
+++ src/modules/daybook/views/NoEntrySelected.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <h1 class="align-self-center">No hay nada seleccionado</h1>
+  </div>
+
+  <Fab />
+</template>
+
+<script>
+import { defineAsyncComponent } from "@vue/runtime-core";
+export default {
+  components: {
+    Fab: defineAsyncComponent(() => import("../components/Fab.vue")),
+  },
+};
+</script>
+
+<style scoped lang="scss">
+div {
+  height: 100vh;
+}
+</style>
