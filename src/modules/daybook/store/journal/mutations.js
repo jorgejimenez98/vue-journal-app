fed4ffedc335ac851cab7af3efd3dc6566292a26
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-03-20 23:18:49 +0100

    Add update entrie mutation

--- src/modules/daybook/store/journal/mutations.js
+++ src/modules/daybook/store/journal/mutations.js
@@ -1,6 +1,9 @@
 export const setEntries = (state, entries) => {
-  state.entries = { ...state.entries, ...entries };
+  state.entries = [...state.entries, ...entries];
   state.isLoading = false;
 };
-export const updateEntrie = (/* state */) => {};
+export const updateEntrie = (state, entry) => {
+  const idx = state.entries.map((item) => item.id).indexOf(entry.id);
+  state.entries[idx] = entry;
+};
 export const createEntrie = (/* state */) => {};
