f8a089030beae61de37676d841c49c20aedee2ae
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-03-20 22:51:11 +0100

    Set entries state to empty array

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,23 +1,4 @@
 export default () => ({
   isLoading: true,
-  entries: [
-    {
-      id: '1',// new Date().getTime(), //12312445242,
-      date: new Date().toDateString(), // sab, 27, julio,
-      text: 'Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO Hola MUNDO ',
-      picture: null,
-    },
-    {
-      id: '2', //new Date().getTime() + 1000, //12312445242,
-      date: new Date().toDateString(), // sab, 27, julio,
-      text: 'PELICULAS PELICULAS PELICULAS PELICULAS PELICULAS PELICULAS PELICULAS PELICULAS PELICULAS ',
-      picture: null,
-    },
-    {
-      id: '3', //new Date().getTime() + 2000, //12312445242,
-      date: new Date().toDateString(), // sab, 27, julio,
-      text: 'JUEGO DE TRONOS JUEGO DE TRONOS JUEGO DE TRONOS JUEGO DE TRONOS JUEGO DE TRONOS ',
-      picture: null,
-    },
-  ],
+  entries: [],
 });
