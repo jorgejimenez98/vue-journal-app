3cece7c433eef79cbbdfdf788cf9f31e75792e97
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-03-28 00:16:35 +0200

    Add entry view unit tests

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -135,7 +135,7 @@ export default {
       });
 
       if (result.isConfirmed) {
-        new Swal({
+        Swal.fire({
           title: "Espere por favor",
           allowOutsideClick: false,
         });
